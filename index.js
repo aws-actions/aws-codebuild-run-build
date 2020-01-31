9060d690f295aa65beb0d5efde8938b4fa38f7f5
Matt Bullock <bullocm@amazon.com>
2020-01-31 13:44:40 -0800

    fix: add log entries before and after CodeBuild activity to differentiate GitHub log activity from CodeBuild log activity (#22)
     add log entries before and after CodeBuild activity
    to differentiate GitHub log activity from CodeBuild log activity.

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ if (require.main === module) {
 module.exports = run;
 
 async function run() {
+  console.log("*****STARTING CODEBUILD*****");
   try {
     const build = await runBuild();
     core.setOutput("aws-build-id", build.id);
@@ -24,5 +25,7 @@ async function run() {
     );
   } catch (error) {
     core.setFailed(error.message);
+  } finally {
+    console.log("*****CODEBUILD COMPLETE*****");
   }
 }
