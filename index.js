1c1c04c8ab8ef96c9bcf92b02939c768f85d6877
Raman Sah <ramansah@live.com>
2022-06-06 20:18:51 +0530

    2.0.2

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 
 const core = require("@actions/core");
 const { runBuild } = require("./code-build");
-const assert = require("assert");
 
 /* istanbul ignore next */
 if (require.main === module) {
@@ -17,12 +16,6 @@ async function run() {
   try {
     const build = await runBuild();
     core.setOutput("aws-build-id", build.id);
-
-    // Signal the outcome
-    assert(
-      build.buildStatus === "SUCCEEDED",
-      `Build status: ${build.buildStatus}`
-    );
   } catch (error) {
     core.setFailed(error.message);
   } finally {
